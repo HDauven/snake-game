457569af1ad6a025418cdfd75f663a98bb3276a0
Hein <heindauven@gmail.com>
2022-06-22 18:04:18 +0200

    Implement display trait for identification.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,25 @@
+use std::fmt;
+
 pub trait Log {
     fn display_info(&self);
 }
 
-#[derive(Debug)]
 pub enum PersonId {
     Passport(String),
-    IdentityCard(String),
+    IdentityCard(String, String, String),
+}
+
+impl fmt::Display for PersonId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PersonId::Passport(x) => {
+                write!(f, "{}", x)
+            }
+            PersonId::IdentityCard(x, y, z) => {
+                write!(f, "{} {} {}", x, y, z)
+            }
+        }
+    }
 }
 
 pub struct Person {
@@ -25,10 +39,7 @@ impl Log for Animal {
 
 impl Log for Person {
     fn display_info(&self) {
-        println!(
-            "{} {} {} {:?}",
-            self.name, self.last_name, self.age, self.id
-        );
+        println!("{} {} {} {}", self.name, self.last_name, self.age, self.id);
     }
 }
 
