d1dbee3908f7f6a780a456df31c2d5f1b14b7d43
Hein <heindauven@gmail.com>
2022-06-25 11:28:25 +0200

    Add bindings for JS log and alert.

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,13 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    println!("Hi there {}", name);
+    log(name);
+}
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
 }
